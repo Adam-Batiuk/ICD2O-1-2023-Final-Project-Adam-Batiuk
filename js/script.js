3e5f4186ad16f2c951e3eeed737dc89a9bbe021c
Adam-Batiuk <159185155+Adam-Batiuk@users.noreply.github.com>
2024-05-30 21:33:10 +0000

    layout

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Copyright (c) 2024 Mr. Adam Batiuk All rights reserved
+//
+// Created by: Adam Batiuk
+// Created on: Mar 2024
+// This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked() {
+    // define numbers
+    const number = parseInt(document.getElementById("your-number").value)
+    const sum = (number / 2) * (number + 1)
+    // print answer
+    document.getElementById("answer").innerHTML = "The answer is: " + sum
+  }
