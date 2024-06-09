dd754ff90b8d623a2199553f6cb5db9c3ca82d60
Adam-Batiuk <159185155+Adam-Batiuk@users.noreply.github.com>
2024-06-09 21:50:14 +0000

    calc

--- js/script.js
+++ js/script.js
@@ -10,7 +10,8 @@ let hasDecimal = false
 let newOperand = true
 let firstOperand = 0
 let secondOperand = 0
-let operator = '\0'
+let operator = "\0"
+let maxDigits = 12
 
 function dis(val) {
   if (val == ".") {
@@ -21,12 +22,11 @@ function dis(val) {
     }
   } else {
     if (newOperand) {
-       document.getElementById("result").innerHTML = val 
-       newOperand = false
-    } else {
+      document.getElementById("result").innerHTML = val
+      newOperand = false
+    } else if (document.getElementById("result").innerHTML.length < maxDigits) {
       document.getElementById("result").innerHTML += val
     }
-      
   }
 }
 
@@ -43,7 +43,6 @@ function addClicked() {
   solve()
   operator = "+"
   firstOperand = parseFloat(document.getElementById("result").innerHTML)
-  
 }
 
 function subtractClicked() {
@@ -80,18 +79,21 @@ function solve() {
     console.log("no operator")
     return
   }
+  if (parseInt(result).toString().length > maxDigits) {
+    console.log("too big")
+    result = "err"
+  } else {
+    result = parseFloat(parseFloat(result).toPrecision(maxDigits))
+  }
   document.getElementById("result").innerHTML = result
   return result
-
 }
 
 function solveClicked() {
   solve()
-  firstOperand = 0 
+  firstOperand = 0
   operator = "\0"
-
 }
-
 
 function doAdd(firstOperand, secondOperand) {
   return firstOperand + secondOperand
@@ -102,7 +104,63 @@ function doSubtract(firstOperand, secondOperand) {
 }
 
 function doMultuply(firstOperand, secondOperand) {
-  return firstOperand * secondOperand
+  let answer = 0
+  let answerDec = 0
+  let isNegitive = false
+  if (firstOperand < 0) {
+    firstOperand = 0 - firstOperand
+    isNegitive = !isNegitive
+  }
+  if (secondOperand < 0) {
+    secondOperand = 0 - secondOperand
+    isNegitive = !isNegitive
+  }
+
+  if (firstOperand.toString().indexOf(".") == -1) {
+    firstOperand = firstOperand + "."
+  }
+
+  let firstOperandInt = firstOperand
+    .toString()
+    .substring(0, firstOperand.toString().indexOf("."))
+  let firstOperandDec = firstOperand
+    .toString()
+    .substring(firstOperand.toString().indexOf(".") + 1)
+  //console.log("I:" + firstOperandInt)
+  //console.log("D:" + firstOperandDec)
+  //console.log(firstOperand.toString().indexOf("."))
+
+  let digit = ""
+  while (firstOperandInt.length > 0) {
+    answer = answer * 10
+    digit = firstOperandInt.toString().substring(0, 1)
+    //console.log("digit: " + digit)
+    for (let idx = 0; idx < digit; idx++) {
+      answer = answer + secondOperand
+      //console.log("answer: " + answer)
+    }
+    firstOperandInt = firstOperandInt.toString().substring(1)
+  }
+
+  let digitDec = ""
+  while (firstOperandDec.length > 0) {
+    digitDec = firstOperandDec.toString().substring(firstOperandDec.length - 1)
+    //console.log("digitDec: " + digitDec)
+    for (let idx = 0; idx < digitDec; idx++) {
+      answerDec = answerDec + secondOperand
+      //console.log("answerDec: " + answerDec)
+    }
+    answerDec = answerDec / 10
+    //console.log("answerDec: " + answerDec)
+    firstOperandDec = firstOperandDec
+      .toString()
+      .substring(0, firstOperandDec.length - 1)
+  }
+  answer = answer + answerDec
+  if (isNegitive == true) {
+    answer = 0 - answer
+  }
+  return answer
 }
 
 function doDivide(firstOperand, secondOperand) {
